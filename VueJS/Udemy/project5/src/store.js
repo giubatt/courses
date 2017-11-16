a43123bbbe64c908eae3a3ed84b3c2dc32fbbe44
Giuseppe <bartgb00@gmail.com>
2017-11-16 20:12:16 -0200

    Logout

--- VueJS/Udemy/project5/src/store.js
+++ VueJS/Udemy/project5/src/store.js
@@ -20,6 +20,10 @@ export default new Vuex.Store({
     storeUser(state, user) {
       state.user = user
     },
+    clearAuthData(state) {
+      state.idToken = null
+      state.userId = null
+    },
   },
   actions: {
     signup({ commit, dispatch }, authData) {
@@ -55,6 +59,9 @@ export default new Vuex.Store({
         })
         .catch(err => console.log(err))
     },
+    logout({ commit }) {
+      commit('clearAuthData')
+    },
     storeUser({ commit, state }, userData) {
       if (!state.idToken) return
 
@@ -83,6 +90,9 @@ export default new Vuex.Store({
   getters: {
     user(state) {
       return state.user
+    },
+    isAuthenticated(state) {
+      return state.idToken !== null
     },
   },
 })
