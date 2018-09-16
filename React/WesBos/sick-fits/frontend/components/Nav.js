2f03a89cd4d80d4be4ad60dc28fa8ba5c8d944c8
Giuseppe <bartgb00@gmail.com>
2018-09-16 01:42:18 -0300

    Querying items and displaying

--- React/WesBos/sick-fits/frontend/components/Nav.js
+++ React/WesBos/sick-fits/frontend/components/Nav.js
@@ -4,15 +4,8 @@ import NavStyles from './styles/NavStyles'
 
 const Nav = () => (
   <NavStyles>
-    <Link href="/">
-      <a>Home!</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
-
     <Link href="/items">
-      <a>Items</a>
+      <a>Shop</a>
     </Link>
     <Link href="/sell">
       <a>Sell</a>
