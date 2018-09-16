2b5b120a8e5bc207659584df8c9f0b176897a033
Giuseppe <bartgb00@gmail.com>
2018-09-16 00:57:51 -0300

    Apollo client connected

--- React/WesBos/sick-fits/frontend/lib/withData.js
+++ React/WesBos/sick-fits/frontend/lib/withData.js
@@ -1,9 +1,11 @@
 import withApollo from 'next-with-apollo'
 import ApolloClient from 'apollo-boost'
+
 import { endpoint } from '../config'
 
 function createClient({ headers }) {
   return new ApolloClient({
+    connectToDevTools: true,
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
     request: operation => {
       operation.setContext({
